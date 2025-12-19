12ef709bfd029215016f22902e00208a6301ca34
VishalDesai <desaidesai62230@gmail.com>
2025-12-19 20:48:44 +0530

    remove serveless

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
-const serverless = require("serverless-http");
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -279,4 +279,3 @@ process.on('uncaughtException', (error) => {
 // ========================================
 // EXPORT FOR VERCEL
 // ========================================
-module.exports = serverless(app);
